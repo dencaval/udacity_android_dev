8229dd55f407f8be506079c6a9a1222e91addd0f
dencaval <dencaval@gmail.com>
2016-09-04 09:35:31 -0300

    Fixes udacity review

--- MyPortifolioApp/app/src/main/java/com/app/portifolio/dencaval/myportifolioapp/MainActivity.java
+++ MyPortifolioApp/app/src/main/java/com/app/portifolio/dencaval/myportifolioapp/MainActivity.java
@@ -10,13 +10,13 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> appNameList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayList<String> appNameList = new ArrayList<String>();
+        appNameList = new ArrayList<String>();
         appNameList.add("Popular Movies");
         appNameList.add("Stock Hawk");
         appNameList.add("Build It Bigger");
@@ -24,23 +24,36 @@ public class MainActivity extends AppCompatActivity {
         appNameList.add("Go Ubiquitous");
         appNameList.add("Capstone");
 
-        for (int i = 0; i < appNameList.size(); i++){
-            final int current = i;
-            String button_text = appNameList.get(current);
-            final String toast_text = "This button will launch my ".concat(button_text)
-                    .concat(" app!");
+//        for (int i = 0; i < appNameList.size(); i++){
+//            final int current = i;
+//            String button_text = appNameList.get(current);
+//            final String toast_text = "This button will launch my ".concat(button_text)
+//                    .concat(" app!");
+//
+//            int resId = getResources().getIdentifier("button".concat(Integer.toString(i+1)),
+//                    "id", getPackageName());
+//            Button b = (Button) findViewById(resId);
+//            b.setText(button_text);
+//
+//            b.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Toast.makeText(getApplicationContext(), toast_text, Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//        }
+    }
 
-            int resId = getResources().getIdentifier("button".concat(Integer.toString(i+1)),
-                    "id", getPackageName());
-            Button b = (Button) findViewById(resId);
-            b.setText(button_text);
+    // You can use this alternative way for the implementation of event using android:onClick attribute
+    // instead of the implementation of event at Activity code.
+    public void showToast(View view){
+        String resource_name = view.getResources().getResourceName(view.getId());
+        Button b = (Button) findViewById(view.getId());
+        int button_pos = Integer.parseInt(resource_name.split("button")[1], 10);
+        String button_text = appNameList.get(button_pos-1);
+        String toast_text = "This button will launch my ".concat(button_text).concat(" app!");
 
-            b.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(getApplicationContext(), toast_text, Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
+        b.setText(button_text);
+        Toast.makeText(getApplicationContext(), toast_text, Toast.LENGTH_SHORT).show();
     }
 }
